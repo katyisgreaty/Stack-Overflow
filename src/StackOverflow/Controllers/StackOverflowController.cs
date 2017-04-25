5047f682f5ef695d2db2dd21548bc653ad187caf
Cassie Musolf and Katy Daviscourt <cassie+katy@>
2017-04-25 10:00:39 -0700

    add authentication and create question.

---
+++ src/StackOverflow/Controllers/StackOverflowController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using StackOverflow.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace StackOverflow.Controllers
+{
+    [Authorize]
+    public class StackOverflowController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public StackOverflowController (UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            return View(_db.Questions.Where(x => x.User.Id == currentUser.Id));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Question question)
+        {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            question.User = currentUser;
+            _db.Questions.Add(question);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ src/StackOverflow/Models/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace StackOverflow.Models
+{
+    [Table("Questions")]
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        [DefaultValue(0)]
+        public int Vote { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
